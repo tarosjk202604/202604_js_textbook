0fddad26e3a1489afbac5b0ba415842993af8d63
tarosjk3 <tarosjk3@gmail.com>
2026-06-05 09:58:54 +0900

    4-9まで完了

--- chap4/count/js/script.js
+++ chap4/count/js/script.js
@@ -6,4 +6,15 @@ text.addEventListener('keyup', () => {
     // テキストエリア(text)のvalue属性値(入力した文字)を取得し、さらにその文字数(length)を取得
     // それをspan(count)の内容として反映
     count.textContent = text.value.length;
+
+    // もし(定数 text の文字数が100文字を超えるなら) {
+    if (text.value.length > 100) {
+        // 定数 count にクラス名 alert を追加する
+        count.classList.add('alert');
+
+        // } そうでないなら(100文字以下なら) {
+    } else {
+        // 定数 count からクラス名 alert を削除する
+        count.classList.remove('alert');
+    }
 });
